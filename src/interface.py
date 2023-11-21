69571bd4589e6f0e9812e55acb9fba9934da10b5
MeMartijn <martijn.schouten@icloud.com>
2023-11-21 20:09:34 +0100

    chore: only display information if there is a query

--- src/interface.py
+++ src/interface.py
@@ -16,39 +16,38 @@ if query:
         query
     )
 
+    st.write("### ✅ Antwoord", )
+    st.info(f"**{result['result']}**")
 
-st.write("### ✅ Antwoord", )
-st.info(f"**{result['result']}**")
+    st.balloons()
 
-st.balloons()
+    st.divider()
 
-st.divider()
+    st.write("_Bronnen:_")
 
-st.write("_Bronnen:_")
+    for source in set(list(map(lambda x: x["source"], result["source_documents"]))):
+        title = answering.get_webpage_title(source)
+        st.write(f"[{title}]({source})", language="markdown", line_numbers=False)
 
-for source in set(list(map(lambda x: x["source"], result["source_documents"]))):
-    title = answering.get_webpage_title(source)
-    st.write(f"[{title}]({source})", language="markdown", line_numbers=False)
+    # st.write("_Mogelijke vervolgvragen_")
 
-# st.write("_Mogelijke vervolgvragen_")
+    # if query:
+    #     new_questions = answering.get_new_questions_from_llm(
+    #         query
+    #     )
 
-# if query:
-#     new_questions = answering.get_new_questions_from_llm(
-#         query
-#     )
+    # st.write("### Vervolgvragen: \n", f"**{new_questions['result']}**")
 
-# st.write("### Vervolgvragen: \n", f"**{new_questions['result']}**")
+    # st.write("_Feedback_")
 
-# st.write("_Feedback_")
+    # feedback = st.text_input(
+    #     "Gaf dit antwoord op je vraag? Zo nee, welke informatie mist u?", ""
+    # )
 
-# feedback = st.text_input(
-#     "Gaf dit antwoord op je vraag? Zo nee, welke informatie mist u?", ""
-# )
-
-# if feedback:
-#     reply = answering.post_feedback(
-#         query,
-#         feedback
-#     )
-#     print(reply)
-#     st.write(reply)
+    # if feedback:
+    #     reply = answering.post_feedback(
+    #         query,
+    #         feedback
+    #     )
+    #     print(reply)
+    #     st.write(reply)
